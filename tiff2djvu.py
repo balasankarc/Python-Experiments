966e90489f4f4defd7f2a2e7071f9eaf599f2ca9
Balasankar C <c.balasankar@gmail.com>
2012-12-28 19:05:29 +0530

    Moving dialog boxes to local

--- tiff2djvu.py
+++ tiff2djvu.py
@@ -5,75 +5,105 @@ import os
 
 
 class Base:
-	
-	def destroy(self,widget,data=None):					#Exit Button
+	#Exit Button
+	def destroy(self,widget,data=None):					
+		if os.path.isdir("tmp"):
+                  os.removedirs("tmp");
 		sys.exit()			
 	
-	def outputselect(self,widget):								#Select output file		
-		self.response = self.dialog1.run()
-		self.outputfilename=self.dialog1.get_filename()
-		self.dialog1.destroy()
+	#Select output file
+	def outputselect(self,widget):
+	        dialog1=gtk.FileChooserDialog("Name Output File",None,gtk.FILE_CHOOSER_ACTION_SAVE,(gtk.STOCK_CANCEL,gtk.RESPONSE_CANCEL,gtk.STOCK_OPEN,gtk.RESPONSE_OK))
+	        dialog1.set_default_response(gtk.RESPONSE_OK)
+	        filter = gtk.FileFilter()
+               	filter.set_name("djvu Files")										
+               	filter.add_pattern("*.djvu")
+        	dialog1.add_filter(filter)
+		response = dialog1.run()
+		self.outputfilename=dialog1.get_filename()
+		dialog1.destroy()
 		
-	def selectfile(self,widget):									#Selecting input files
-		self.response=""
-		self.response = self.dialog.run()
-		if self.response == gtk.RESPONSE_OK:
-			self.filelist = self.dialog.get_filenames()		
-		self.dialog.destroy()		
-		
+	#Selecting input files	
+	def selectfile(self,widget):
+	        dialog=gtk.FileChooserDialog("Select Input Files",None,gtk.FILE_CHOOSER_ACTION_OPEN,(gtk.STOCK_CANCEL,gtk.RESPONSE_CANCEL,gtk.STOCK_OPEN,gtk.RESPONSE_OK))
+	        dialog.set_default_response(gtk.RESPONSE_OK)
+	        dialog.set_select_multiple(True)
+        	filter = gtk.FileFilter()
+        	filter.set_name("Tiff Files")									
+        	filter.add_pattern("*.tif")
+        	dialog.add_filter(filter)
+		response = dialog.run()
+		if response == gtk.RESPONSE_OK:
+			self.filelist = dialog.get_filenames()		
+		dialog.destroy()	
+	
+        #Converting tif files to individual pdf files		
 	def convert(self,widget):										
-		folder = "/home/tmp/"
-		cmnd0="mkdir ~/Desktop/tmp"
-		flag=os.system(cmnd0)	
+		flag=1
+		if not os.path.isdir("tmp"):
+		  print"creating tmp folder"
+		  os.makedirs("tmp");
 		j=100
 		for p in self.filelist:		
 			print p
-			cmnd = "tiff2pdf -o ~/Desktop/tmp/"+str(j)+"_temp.pdf "+ p			#Converting tif files to individual pdf files
+			cmnd = "tiff2pdf -o tmp/"+str(j)+"_temp.pdf "+ p			
 			flag=os.system(cmnd)
 			j=j+1
+		md1= gtk.MessageDialog(None,gtk.DIALOG_DESTROY_WITH_PARENT, gtk.MESSAGE_ERROR,gtk.BUTTONS_CLOSE, "Error while creating pdf")
 		if flag!=0:
-			self.md1.run()
-			self.md1.destroy()
-			sys.exit()
-		cmnd2="pdftk ~/Desktop/tmp/*.pdf cat output ~/Desktop/tmp/file_merged.pdf"		#Merging pdf files to a single pdf files
+			md1.run()
+			md1.destroy()
+			return
+                #Merging pdf files to a single pdf files
+                md1= gtk.MessageDialog(None,gtk.DIALOG_DESTROY_WITH_PARENT, gtk.MESSAGE_ERROR,gtk.BUTTONS_CLOSE, "Error while merging pdf")
+		cmnd2="pdftk tmp/*.pdf cat output tmp/file_merged.pdf"		
 		flag=os.system(cmnd2)
 		if flag!=0:
-			self.md1.run()
-			self.md1.destroy()
-			sys.exit()
-		cmnd3="pdf2djvu -o "+self.outputfilename+".djvu ~/Desktop/tmp/file_merged.pdf"		#Converting pdf file to djvu file
+			md1.run()
+			md1.destroy()
+			return
+                #Converting pdf file to djvu file
+                md1= gtk.MessageDialog(None,gtk.DIALOG_DESTROY_WITH_PARENT, gtk.MESSAGE_ERROR,gtk.BUTTONS_CLOSE, "Error while creating djvu")			
+		cmnd3="pdf2djvu -o "+self.outputfilename+".djvu tmp/file_merged.pdf"		
 		flag=os.system(cmnd3)
 		if flag!=0:
-			self.md1.run()
-			self.md1.destroy()
-			sys.exit()
+			md1.run()
+			md1.destroy()
+			return
 		j=100
 		for p in self.filelist:
-			cmnd4="rm ~/Desktop/tmp/"+str(j)+"_temp.pdf"						#Removing unwanted individual pdf files
+                        #Removing unwanted individual pdf files
+                        md1= gtk.MessageDialog(None,gtk.DIALOG_DESTROY_WITH_PARENT, gtk.MESSAGE_ERROR,gtk.BUTTONS_CLOSE, "Error while removing pdfs")
+			cmnd4="rm tmp/"+str(j)+"_temp.pdf"						
 			flag=os.system(cmnd4)
 			j=j+1
 		if flag!=0:
-			self.md1.run()
-			self.md1.destroy()
-			sys.exit()
-		cmnd5="rm ~/Desktop/tmp/file_merged.pdf"									#Removing  unwanted merged pdf file
+			md1.run()
+			md1.destroy()
+			return
+		cmnd5="rm tmp/file_merged.pdf"									
+		#Removing  unwanted merged pdf file
 		flag=os.system(cmnd5)
-		cmnd6 = "rmdir ~/Desktop/tmp"													#Removing temp directory
-		flag=os.system(cmnd6)
+		#Removing temp directory
+		cmnd6 = "rmdir tmp"		
+		if os.path.isdir("tmp"):
+		  flag=os.system(cmnd6)
 		if flag!=0:
-			self.md1.run()
-			self.md1.destroy()
-			sys.exit()
-		self.md.run()																				#Displaying successful message
+			md1.run()
+			md1.destroy()
+			return
+		md = gtk.MessageDialog(None,gtk.DIALOG_DESTROY_WITH_PARENT, gtk.MESSAGE_INFO,gtk.BUTTONS_CLOSE, "Successfully Converted")
+		md.run()		
+		#Displaying successful message
 		self.md.destroy()
 		
-	def __init__(self):																			#Main function
+	def __init__(self):
+	        #Main function
 		self.flag=0
 		self.window = gtk.Window()
 		self.window.set_position(gtk.WIN_POS_CENTER)
-		self.windowx,self.windowy = self.window.get_position()
-		self.window.maximize()
-		self.windowheight,self.windowwidth = self.window.get_size()		
+		self.windowx,self.windowy = self.window.get_position()		
+		self.windowheight,self.windowwidth = self.window.get_size()				
 		self.window.show()
 		self.fixed = gtk.Fixed()
 		self.window.connect("destroy",self.destroy)
@@ -85,11 +115,6 @@ class Base:
 		self.button4.connect("clicked",self.destroy)		
 		self.button1.connect("clicked",self.selectfile)
 		self.button2.connect("clicked",self.outputselect)
-		self.dialog1=gtk.FileChooserDialog("Name Output File",None,gtk.FILE_CHOOSER_ACTION_SAVE,(gtk.STOCK_CANCEL,gtk.RESPONSE_CANCEL,gtk.STOCK_OPEN,gtk.RESPONSE_OK))
-		self.dialog=gtk.FileChooserDialog("Select Input Files",None,gtk.FILE_CHOOSER_ACTION_OPEN,(gtk.STOCK_CANCEL,gtk.RESPONSE_CANCEL,gtk.STOCK_OPEN,gtk.RESPONSE_OK))
-		self.dialog.set_default_response(gtk.RESPONSE_OK)
-		self.filter = gtk.FileFilter()
-		self.dialog1.set_default_response(gtk.RESPONSE_OK)
 		self.button3.set_size_request(150,50)				
 		self.button1.set_size_request(150,50)	
 		self.button2.set_size_request(150,50)	
@@ -97,11 +122,8 @@ class Base:
 		self.fixed.put(self.button1,30,30)
 		self.fixed.put(self.button2,30,100)
 		self.fixed.put(self.button4,30,240)		
-		self.fixed.put(self.button3,30,170)		
-		self.md = gtk.MessageDialog(None,gtk.DIALOG_DESTROY_WITH_PARENT, gtk.MESSAGE_INFO,gtk.BUTTONS_CLOSE, "Successfully Converted")
-		self.md1= gtk.MessageDialog(None,gtk.DIALOG_DESTROY_WITH_PARENT, gtk.MESSAGE_ERROR,gtk.BUTTONS_CLOSE, "Error")
+		self.fixed.put(self.button3,30,170)
 		self.filelist=""
-		self.dialog.set_select_multiple(True)
 		self.window.add(self.fixed)
 		self.window.show_all()
 	def main(self):
@@ -110,3 +132,4 @@ class Base:
 if __name__ == "__main__":
 	base= Base()
 	base.main()
+
